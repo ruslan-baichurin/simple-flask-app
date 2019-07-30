1f27701123222326284b8651180238837b96cf1b
Ruslan <ruslan.b1976@gmail.com>
2019-07-30 04:01:20 -0400

    Initial commit
    A simple web app using Flask Web Framework

---
+++ program/routes.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from program import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/100days')
+def hundred_days():
+    return render_template('100days.html')
